20b5d37d766c2eebaaa2fc183478b5f478564eb1
jordimonk <jordimonk@yahoo.com>
2022-01-13 22:47:01 -0700

    more changes

--- serverless/queries.js
+++ serverless/queries.js
@@ -20,7 +20,7 @@ const get10FirstParks = (request, response) => {
 };
 
 const getCentroids = async () => {
-  return { message: "All DATA RETRIEVED" };
+  return { user: user };
   return pool.query(
     'SELECT name AS "Park Name", ST_Y(ST_CENTROID(ST_TRANSFORM(geometry, 4326))) AS latitude, ST_X(ST_CENTROID(ST_TRANSFORM(geometry, 4326))) AS longitude FROM parks_protected_areas LIMIT 10',
     (error, results) => {
