6bef2a7f9a6c3e4d07e06f2ee52f37d82ffda169
jordimonk <jordimonk@yahoo.com>
2022-01-18 22:22:55 -0700

    Fixed query for dissemination areas

--- serverless/queries.js
+++ serverless/queries.js
@@ -35,7 +35,7 @@ const getDissemAreas = async () => {
     const res = await pool.query(
       `SELECT id AS "ID",
         dissem_area_uid AS "Dissemination Area ID",
-        population AS "Population",
+        population,
         ST_Y(ST_CENTROID(ST_TRANSFORM(geom, 4326))) AS latitude,
         ST_X(ST_CENTROID(ST_TRANSFORM(geom, 4326))) AS longitude
         FROM public.statscan_dissemination_areas
