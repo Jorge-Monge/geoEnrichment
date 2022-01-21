3a163397322af18420e29bc6adb6b30d4f718f21
jordimonk <jordimonk@yahoo.com>
2022-01-20 21:26:02 -0700

    first spatial query

--- serverless/queries.js
+++ serverless/queries.js
@@ -87,9 +87,9 @@ const buildQuery = (geometry_parameter) => {
 
 const spitPopulation = async (WKT_geometry) => {
   try {
-    //const res = await pool.query(buildQuery(WKT_geometry));
-    //return res.rows;
-    return buildQuery(WKT_geometry);
+    const res = await pool.query(buildQuery(WKT_geometry));
+    return res.rows;
+    //return buildQuery(WKT_geometry);
   } catch (err) {
     return { error: err.stack };
   }
